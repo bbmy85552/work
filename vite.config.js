9b94c0b30201e41f5d61cb463e7556e845d59a5e
2dqy <info@2dqy.com>
2025-12-17 00:54:35 +0800

    fix vercel img error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    // 确保 public 目录下的文件被正确复制
+    copyPublicDir: true,
+  },
 })
